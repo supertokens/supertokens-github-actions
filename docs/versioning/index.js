56cf12ceb48c9d6460a2f06d794c2f4514d6ba6d
Nemi Shah <nemishah1212@gmail.com>
2023-09-15 13:02:00 +0530

    testing

--- docs/versioning/index.js
+++ docs/versioning/index.js
@@ -1,10 +1,7 @@
 import Github from "@actions/github";
 import { readdirSync } from "fs";
-import fetch from "node-fetch";
 import path from "path";
-import tar from "tar";
-import stream from "node:stream";
-import { promisify } from "util";
+import clone from "git-clone/promise";
 
 const pipeline = promisify(stream.pipeline);
 
@@ -15,46 +12,16 @@ console.log("");
 console.log("");
 
 async function start() {
-    const octokit = Github.getOctokit(process.env.INPUT_GITHUB_TOKEN);
+    // const octokit = Github.getOctokit(process.env.INPUT_GITHUB_TOKEN);
     // const tags = (await octokit.rest.repos.listTags({
     //     owner: process.env.INPUT_GITHUB_OWNER,
     //     repo: "docs"
     // })).data;
 
     // download entire repo from github
-    const downloadURL = `https://github.com/${process.env.INPUT_GITHUB_OWNER}/docs/archive/master.tar.gz`;
-    console.log("Downloading from: " + downloadURL);
-
-    const downloadResponse = await fetch(downloadURL);
-    console.log("Repsonse", downloadResponse.status, downloadResponse.statusText)
-    await pipeline(
-        downloadResponse.body,
-        tar.extract({
-            filter: (path) => {
-                if (path.includes("codeTypeChecking")) {
-                    return true;
-                }
-
-                return false;
-            },
-        }, [])
-    )
-
-    pipeline(
-        downloadResponse.body,
-        tar.extract(
-            {
-                strict: true,
-                filter: (path, _) => {
-                    if (path.includes("codeTypeChecking")) {
-                        return true;
-                    }
+    const downloadURL = `https://github.com/${process.env.INPUT_GITHUB_OWNER}/docs.git`;
     
-                    return false;
-                },
-            },
-        )
-    );
+    await clone(downloadURL, "./clone");
 
     readdirSync(path.resolve(process.cwd(), "./"));
 }
