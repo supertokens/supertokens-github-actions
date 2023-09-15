bdc7d47d111754ae9de3efcba038d3ef7bef5ccc
Nemi Shah <nemishah1212@gmail.com>
2023-09-15 11:58:28 +0530

    Testing

--- docs/versioning/index.js
+++ docs/versioning/index.js
@@ -1,10 +1,19 @@
 import Github from "@actions/github";
+import fs from "fs";
 
-console.log(process.env.INPUT_GITHUB_OWNER)
-const octokit = Github.getOctokit(process.env.INPUT_GITHUB_TOKEN);
-const tags = await octokit.rest.repos.listTags({
-    owner: process.env.INPUT_GITHUB_OWNER,
-    repo: "docs"
-});
-console.log("TAGSSSS");
-console.log(tags);
+console.log("Environment variables:");
+console.log("OWNER: " + process.env.INPUT_GITHUB_OWNER);
+console.log("--------------");
+console.log("");
+console.log("");
+
+async function start() {
+    const octokit = Github.getOctokit(process.env.INPUT_GITHUB_TOKEN);
+    const tags = (await octokit.rest.repos.listTags({
+        owner: process.env.INPUT_GITHUB_OWNER,
+        repo: "docs"
+    })).data;
+
+    console.log(process.cwd());
+    console.log(fs.readdirSync(process.cwd()));
+}
