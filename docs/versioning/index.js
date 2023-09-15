0194140844cd00e045d0abc4160a27c118be839d
Nemi Shah <nemishah1212@gmail.com>
2023-09-15 13:18:22 +0530

    testing

--- docs/versioning/index.js
+++ docs/versioning/index.js
@@ -18,7 +18,36 @@ async function start() {
 
     const jsPackageJsonPath = path.resolve(process.cwd(), "./v2/src/plugins/codeTypeChecking/jsEnv/package.json");
     const jsEnvPackageJson = JSON.parse(readFileSync(jsPackageJsonPath, "utf-8"));
-    console.log(jsEnvPackageJson)
+    
+    const jsDependencies = jsEnvPackageJson.dependencies;
+    const authReactVersion = jsDependencies["supertokens-auth-react"];
+    const nodeVersion = jsDependencies["supertokens-node"];
+    const node7Version = jsDependencies["supertokens-node7"];
+    const reactNativeVersion = jsDependencies["supertokens-react-native"];
+    const webJsVersion = jsDependencies["supertokens-web-js"];
+    const webjsScriptVersion = jsDependencies["supertokens-web-js-script"];
+    const websiteVersion = jsDependencies["supertokens-website"];
+    const websiteScriptVersion = jsDependencies["supertokens-website-script"];
+
+    if (authReactVersion.includes("github:")) {
+        throw new Error("supertokens-auth-react is not using a released version of the SDK.");
+    }
+
+    if (reactNativeVersion.includes("github:")) {
+        throw new Error("supertokens-react-native is not using a released version of the SDK.");
+    }
+
+    if (nodeVersion.includes("github:") || node7Version.includes("github:")) {
+        throw new Error("supertokens-node OR node7 is not using a released version of the SDK.");
+    }
+
+    if (webJsVersion.includes("github:") || webjsScriptVersion.includes("github:")) {
+        throw new Error("supertokens-web-js OR web-js-script is not using a released version of the SDK.");
+    }
+
+    if (websiteVersion.includes("github:") || websiteScriptVersion.includes("github:")) {
+        throw new Error("supertokens-website OR website-script is not using a released version of the SDK.");
+    }
 }
 
 start();
